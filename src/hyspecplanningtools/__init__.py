7537830f5b196d0ff37eacf9a725a40a31b0c4fd
Patrou, Maria <patroum@ornl.gov>
2024-10-28 15:30:49 -0400

    configuration, version, and meta updates for new repo

--- src/hyspecplanningtools/__init__.py
+++ src/hyspecplanningtools/__init__.py
@@ -5,9 +5,3 @@ try:
 except ImportError:
     __version__ = "unknown"
 
-
-def PackageName():  # noqa N802
-    """This is needed for backward compatibility because mantid workbench does "from shiver import Shiver" """
-    from .packagenamepy import PackageName as packagename  # noqa N813
-
-    return packagename()
