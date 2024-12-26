a76429d89ba95ff8504664145acd7b9497e10f76
Kyle Ma <kyleqianlima@gmail.com>
2024-12-26 12:33:10 -0500

    setting get value functions and dummy functions for future implementation

--- src/hyspecppt/hppt/hppt_presenter.py
+++ src/hyspecppt/hppt/hppt_presenter.py
@@ -21,6 +21,11 @@ class HyspecPPTPresenter:
         self.view.CW.set_values(DEFAULT_CROSSHAIR)
         self.view.SelW.set_SC_toggle(True)
 
+        self.get_Experiment_values()
+        self.get_SingleCrystal_values()
+        self.get_Selector_values()
+        self.get_Crosshair_values()
+
     @property
     def view(self):
         """Return the view for this presenter"""
@@ -30,3 +35,56 @@ class HyspecPPTPresenter:
     def model(self):
         """Return the model for this presenter"""
         return self._model
+
+    def get_Experiment_values(self) -> dict:
+        """Get Ei, Pangle, S2, Type values from Experiment
+
+        return: dict of Experiment key value pairs
+        """
+        EW_dict = {}
+        EW_dict["Ei"] = self.view.EW.Ei_edit.text()
+        EW_dict["Pangle"] = self.view.EW.Pangle_edit.text()
+        EW_dict["S2"] = self.view.EW.S2_edit.text()
+        EW_dict["Type"] = self.view.EW.Type_combobox.currentText()
+        return EW_dict
+
+    def get_Selector_values(self):
+        """Check if Single Crystal radio button is checked
+
+        return: True - Single Crystal radio button is toggled
+                False - Single Crystal radio button is not toggled. Powder radio button is toggled
+        """
+        return self.view.SelW.sc_rb.isChecked()
+
+    def get_SingleCrystal_values(self):
+        """Get Single Crystal mode specific values from SingleCrystalWidget
+
+        return: dict of Single Crystal key value pairs
+        """
+        SC_dict = {}
+        SC_dict["a"] = self.view.SCW.a_edit.text()
+        SC_dict["b"] = self.view.SCW.b_edit.text()
+        SC_dict["c"] = self.view.SCW.c_edit.text()
+
+        SC_dict["alpha"] = self.view.SCW.alpha_edit.text()
+        SC_dict["beta"] = self.view.SCW.beta_edit.text()
+        SC_dict["gamma"] = self.view.SCW.gamma_edit.text()
+
+        SC_dict["h"] = self.view.SCW.h_edit.text()
+        SC_dict["k"] = self.view.SCW.k_edit.text()
+        SC_dict["l"] = self.view.SCW.l_edit.text()
+        return SC_dict
+
+    def get_Crosshair_values(self):
+        """Get Crosshair mode specific values from CrosshairWidget
+
+        return: dict of Crosshair key value pairs
+        """
+        CH_dict = {}
+        CH_dict["DeltaE"] = self.view.CW.DeltaE_edit.text()
+        CH_dict["modQ"] = self.view.CW.modQ_edit.text()
+        return CH_dict
+
+    def set_PlotWidget_values(self):
+        """Pass through intensity matrix into plot in view"""
+        pass
