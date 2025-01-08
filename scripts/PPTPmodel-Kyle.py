a0c06ac22ae320702cfc8f4c609e14b46ed1b382
Kyle Qianli Ma <41385391+KyleQianliMa@users.noreply.github.com>
2025-01-08 13:26:14 -0500

    Sc model (#18)
    * SC model inital commit
    
    * uploading original scripts
    
    * setting scripts right
    
    * powder mode
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * updaing model logic
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * changing to follow convention
    
    * getter and setter functions
    
    * minor changes
    
    * set single crystal parameters in model
    
    * set deltaE optional
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ scripts/PPTPmodel-Kyle.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 16 16:28:21 2024
+
+@author: qmc
+"""
+
+import numpy as np
+from scipy.constants import m_n, e, hbar
+import matplotlib.pyplot as plt
+
+SE2K = np.sqrt(2e-3*e*m_n) *1e-10/hbar
+
+class Model:
+    def __init__(self):
+        pass
+
+    def polarization_powder(self,Ei, EMin, S2, alpha_p, plot_options="alpha",left=True):
+        #def Ei, Emin = - Ei to create Qmin, Qmax to generate plot range
+        # Ei=20.0
+        if EMin == None:
+            EMin=-Ei 
+        E = np.linspace(EMin, Ei*0.9, 200)
+        
+        kfmin = np.sqrt(Ei-EMin) * SE2K
+        ki = np.sqrt(Ei) * SE2K
+        
+        # S2= 60
+        #Create Qmin and Qmax
+        Qmax = np.sqrt(ki**2 + kfmin**2 - 2*ki*kfmin*np.cos(np.radians(S2+30))) #Q=ki or Q=
+        Qmin=0
+        Q = np.linspace(Qmin, Qmax, 200)
+        
+        #Create 2D array
+        E2d, Q2d = np.meshgrid(E, Q)
+        
+        Ef2d = Ei-E2d
+        kf2d = np.sqrt(Ef2d) *SE2K
+        
+        # Px=P[0]
+        Px = np.cos(np.radians(alpha_p))
+        # Pz=P[2]
+        Pz = np.sin(np.radians(alpha_p))
+        
+        cos_theta = (ki**2 + kf2d**2 - Q2d**2)/ (2*ki*kf2d)
+        cos_theta[cos_theta < np.cos(np.radians(S2+30))] = np.nan 
+        cos_theta[cos_theta > np.cos(np.radians(S2-30))] = np.nan #cos(30)
+        
+        # cos_theta[cos_theta < -1] = np.nan
+        # cos_theta[cos_theta > 1] = np.nan
+        
+        # theta=np.arccos(cos_theta)
+        
+
+        Qz = ki - kf2d*cos_theta
+        Qx= (-1 if left else 1) * kf2d*np.sqrt((1-cos_theta**2))
+        
+        cos_ang_PQ = (Qx*Px + Qz*Pz)/Q2d/np.sqrt(Px**2+Pz**2)
+        cos_ang_PQ[cos_ang_PQ**2 <0.4] = np.nan 
+        cos_ang_PQ[cos_ang_PQ**2 >0.6] = np.nan
+        ang_PQ = np.degrees(np.arccos(cos_ang_PQ))
+        
+        kf=np.sqrt(Ei-E)*SE2K
+        
+        Q_low = np.sqrt(ki**2 + kf**2 - 2*ki*kf*np.cos(np.radians(S2-30)))
+        Q_hi = np.sqrt(ki**2 + kf**2 - 2*ki*kf*np.cos(np.radians(S2+30)))
+
+        if plot_options == "alpha":
+            return Q_low, Q_hi, E, Q2d, E2d, ang_PQ  
+        
+        if plot_options == "cos^2(a)":
+            return Q_low, Q_hi, E, Q2d, E2d, np.cos(np.radians(ang_PQ))**2
+
+        if plot_options == "cos^2(a)-sin^2(a)":
+            return Q_low, Q_hi, E, Q2d, E2d, np.cos(np.radians(ang_PQ))**2 - np.sin(np.radians(ang_PQ))**2
+        
+        if plot_options == "(cos^2(a)+1)/2":
+            return Q_low, Q_hi, E, Q2d, E2d, (np.cos(np.radians(ang_PQ))**2 +1)/2
+        
+        if plot_options == "alpha":
+            fig, ax = plt.subplots()
+            pcm = ax.pcolormesh(Q2d, E2d, ang_PQ)
+            ax.plot(Q_low, E)
+            ax.plot(Q_hi, E)
+            
+            fig.colorbar(pcm, label= "Degrees")
+            ax.set_xlabel(r"|Q| ($\AA^{-1}$)")
+            ax.set_ylabel("E (meV)")
+            fig.show()
+            
+        if plot_options == "cos^2(a)":
+            fig, ax = plt.subplots()
+            pcm = ax.pcolormesh(Q2d, E2d, np.cos(np.radians(ang_PQ))**2)
+            ax.plot(Q_low, E)
+            ax.plot(Q_hi, E)
+            
+            fig.colorbar(pcm, label= r"$cos^2(\alpha)$")
+            ax.set_xlabel(r"|Q| ($\AA^{-1}$)")
+            ax.set_ylabel("E (meV)")
+            fig.show()
+            
+        if plot_options == "cos^2(a)-sin^2(a)":
+            fig, ax = plt.subplots()
+            pcm = ax.pcolormesh(Q2d, E2d, np.cos(np.radians(ang_PQ))**2 - np.sin(np.radians(ang_PQ))**2)
+            ax.plot(Q_low, E)
+            ax.plot(Q_hi, E)
+            
+            fig.colorbar(pcm, label= r"$cos^2(\alpha) - sin^2(\alpha)$")
+            ax.set_xlabel(r"|Q| ($\AA^{-1}$)")
+            ax.set_ylabel("E (meV)")
+            fig.show()
+
+        if plot_options == "(cos^2(a)+1)/2":
+            fig, ax = plt.subplots()
+            pcm = ax.pcolormesh(Q2d, E2d, (np.cos(np.radians(ang_PQ))**2 +1)/2)
+            ax.plot(Q_low, E)
+            ax.plot(Q_hi, E)
+            
+            fig.colorbar(pcm, label= r"$\dfrac{cos^2(\alpha) - 1}{2}$")
+            ax.set_xlabel(r"|Q| ($\AA^{-1}$)")
+            ax.set_ylabel("E (meV)")
+            fig.show()
+
+if __name__ == "__main__":
+    obj = Model()
+    output = obj.polarization_powder(20, None, 60, 30,plot_options="alpha",left=True)
+    Q_low, Q_hi, E, Q2d, E2d, ang_PQ = output[0], output[1], output[2], output[3], output[4], output[5]
+    
+    fig, ax = plt.subplots()
+    pcm = ax.pcolormesh(Q2d, E2d, ang_PQ)
+    ax.plot(Q_low, E)
+    ax.plot(Q_hi, E)
+    
+    fig.colorbar(pcm, label= "Degrees")
+    ax.set_xlabel(r"|Q| ($\AA^{-1}$)")
+    ax.set_ylabel("E (meV)")
+    plt.title("Ei=20, Emin=None, S2=60, angle_p=30, plot_options = alpha, left=True")
+    fig.show()
+
+# fig.show()
+
+#test case should be -95.6571
+# Ef=30
+# kf=np.sqrt(Ef)*SE2K
+# Qx = -kf*np.sin(np.radians(30))
+# Qz = ki-kf*np.cos(np.radians(30))
+
+# print(np.degrees(np.arctan2(Qx,Qz)))
--- src/hyspecppt/hppt/hppt_model.py
+++ src/hyspecppt/hppt/hppt_model.py
@@ -1,13 +1,214 @@
-"""Model for the Main tab"""
+"""Model for the polarization planning tool"""
 
 import logging
 
+import numpy as np
+from scipy.constants import e, hbar, m_n
+
+from .experiment_settings import PLOT_TYPES
+
 logger = logging.getLogger("hyspecppt")
+
+
+class SingleCrystalParameters:
+    """Model for single crystal calculations"""
+
+    a: float
+    b: float
+    c: float
+    alpha: float
+    beta: float
+    gamma: float
+    h: float
+    k: float
+    l: float
+
+    def __init__(self) -> None:
+        """Constructor"""
+        return
+
+    def set_parameters(self, params: dict[str, float]) -> None:
+        """Store single crystal parameters
+
+        Args:
+            params: dict - contains the following keys:
+                a, b, c, alpha, beta, gamma, h, k, l
+
+        """
+        self.a = params["a"]
+        self.b = params["b"]
+        self.c = params["c"]
+        self.alpha = params["alpha"]
+        self.beta = params["beta"]
+        self.gamma = params["gamma"]
+        self.h = params["h"]
+        self.k = params["k"]
+        self.l = params["l"]
+
+    def get_parameters(self) -> dict[str, float]:
+        """Returns all the parameters as a dictionary"""
+        try:
+            return dict(
+                a=self.a,
+                b=self.b,
+                c=self.c,
+                alpha=self.alpha,
+                beta=self.beta,
+                gamma=self.gamma,
+                h=self.h,
+                k=self.k,
+                l=self.l,
+            )
+        except AttributeError:
+            logger.error("The parameters were not initialized-get_parameters-SingleCrystalParameters")
+
+    def calculate_modQ(self) -> float:
+        """Returns |Q| from lattice parameters and h, k, l"""
+        try:
+            ca = np.cos(np.radians(self.alpha))
+            sa = np.sin(np.radians(self.alpha))
+            cb = np.cos(np.radians(self.beta))
+            sb = np.sin(np.radians(self.beta))
+            cg = np.cos(np.radians(self.gamma))
+            sg = np.sin(np.radians(self.gamma))
+            vabg = np.sqrt(1 - ca**2 - cb**2 - cg**2 + 2 * ca * cb * cg)
+            astar = sa / (self.a * vabg)
+            bstar = sb / (self.b * vabg)
+            cstar = sg / (self.c * vabg)
+            cas = (cb * cg - ca) / (sb * sg)  # noqa: F841
+            cbs = (cg * ca - cb) / (sg * sa)
+            cgs = (ca * cb - cg) / (sa * sb)
+
+            # B matrix
+            B = np.array(
+                [
+                    [astar, bstar * cgs, cstar * cbs],
+                    [0, bstar * np.sqrt(1 - cgs**2), -cstar * np.sqrt(1 - cbs**2) * ca],
+                    [0, 0, 1.0 / self.c],
+                ]
+            )
+
+            modQ = 2 * np.pi * np.linalg.norm(B.dot([self.h, self.k, self.l]))
+            return modQ
+        except AttributeError:
+            logger.error("The parameters were not initialized-calculate_modQ")
+
+
+class CrosshairParameters:
+    """Model for the crosshair parameters"""
+
+    modQ: float
+    DeltaE: float
+    current_experiment_type: str
+    sc_parameters: SingleCrystalParameters
+    experiment_types = ["single_crystal", "powder"]
+
+    def __init__(self):
+        self.sc_parameters = SingleCrystalParameters()
+
+    def set_crosshair(self, current_experiment_type: str, DeltaE: float = None, modQ: float = None) -> None:
+        """Store crosshair parameters including in SC mode"""
+        if current_experiment_type is not None:
+            self.current_experiment_type = current_experiment_type
+        if DeltaE is not None:
+            self.DeltaE = DeltaE
+        if modQ is not None:
+            self.modQ = modQ
+
+    def get_crosshair(self) -> dict[str, float]:
+        """Get the crosshair"""
+        if self.current_experiment_type == "single_crystal":
+            self.modQ = self.sc_parameters.calculate_modQ()
+        return dict(DeltaE=self.DeltaE, modQ=self.modQ)
 
 
 class HyspecPPTModel:
     """Main model"""
 
+    Ei: float = 0
+    S2: float = 0
+    alpha_p: float = 0
+    plot_type: str = ""
+    cp: CrosshairParameters
+
     def __init__(self):
         """Constructor"""
-        return
+        self.cp = CrosshairParameters()
+
+    def set_single_crystal_data(self, params: dict[str, float]) -> None:
+        self.cp.sc_parameters.set_parameters(params)
+
+    def get_single_crystal_data(self) -> dict[str, float]:
+        return self.cp.sc_parameters.get_parameters()
+
+    def set_crosshair_data(self, current_experiment_type: str, DeltaE: float = None, modQ: float = None) -> None:
+        self.cp.set_crosshair(current_experiment_type=current_experiment_type, DeltaE=DeltaE, modQ=modQ)
+
+    def get_crosshair_data(self) -> dict[str, float]:
+        return self.cp.get_crosshair()
+
+    def set_experiment_data(self, Ei: float, S2: float, alpha_p: float, plot_type: str) -> None:
+        self.Ei = Ei
+        self.S2 = S2
+        self.alpha_p = alpha_p
+        self.plot_type = plot_type
+
+    def get_graph_data(self) -> list[float, float, float, list, list, list]:
+        return self.calculate_graph_data()
+
+    def calculate_graph_data(self) -> list[float]:
+        """Returns a list of [Q_low, Q_hi, E, Q2d, E2d, data of plot_types]"""
+        try:
+            SE2K = np.sqrt(2e-3 * e * m_n) * 1e-10 / hbar
+            # def Ei, Emin = - Ei to create Qmin, Qmax to generate plot range
+            if self.cp.DeltaE is not None and self.cp.DeltaE < -self.Ei:
+                EMin = -self.Ei
+            E = np.linspace(EMin, self.Ei * 0.9, 200)
+
+            kfmin = np.sqrt(self.Ei - EMin) * SE2K
+            ki = np.sqrt(self.Ei) * SE2K
+
+            # S2= 60
+            # Create Qmin and Qmax
+            Qmax = np.sqrt(ki**2 + kfmin**2 - 2 * ki * kfmin * np.cos(np.radians(self.S2 + 30)))  # Q=ki or Q=
+            Qmin = 0
+            Q = np.linspace(Qmin, Qmax, 200)
+
+            # Create 2D array
+            E2d, Q2d = np.meshgrid(E, Q)
+
+            Ef2d = self.Ei - E2d
+            kf2d = np.sqrt(Ef2d) * SE2K
+
+            Px = np.cos(np.radians(self.alpha_p))
+            Pz = np.sin(np.radians(self.alpha_p))
+
+            cos_theta = (ki**2 + kf2d**2 - Q2d**2) / (2 * ki * kf2d)
+            cos_theta[cos_theta < np.cos(np.radians(self.S2 + 30))] = np.nan
+            cos_theta[cos_theta > np.cos(np.radians(self.S2 - 30))] = np.nan  # cos(30)
+
+            Qz = ki - kf2d * cos_theta
+
+            if self.S2 >= 30:
+                Qx = (-1) * kf2d * np.sqrt((1 - cos_theta**2))
+            elif self.S2 <= -30:
+                Qx = kf2d * np.sqrt((1 - cos_theta**2))
+
+            cos_ang_PQ = (Qx * Px + Qz * Pz) / Q2d / np.sqrt(Px**2 + Pz**2)
+            ang_PQ = np.degrees(np.arccos(cos_ang_PQ))
+
+            kf = np.sqrt(self.Ei - E) * SE2K
+
+            Q_low = np.sqrt(ki**2 + kf**2 - 2 * ki * kf * np.cos(np.radians(self.S2 - 30)))
+            Q_hi = np.sqrt(ki**2 + kf**2 - 2 * ki * kf * np.cos(np.radians(self.S2 + 30)))
+
+            if self.plot_type == PLOT_TYPES[0]:  # alpha
+                return [Q_low, Q_hi, E, Q2d, E2d, ang_PQ]
+
+            if self.plot_type == PLOT_TYPES[1]:  # cos^2(alpha)
+                return [Q_low, Q_hi, E, Q2d, E2d, np.cos(np.radians(ang_PQ)) ** 2]
+
+            if self.plot_type == PLOT_TYPES[2]:  # "(cos^2(a)+1)/2"
+                return [Q_low, Q_hi, E, Q2d, E2d, (np.cos(np.radians(ang_PQ)) ** 2 + 1) / 2]
+        except AttributeError:
+            logger.error("The parameters were not initialized")
