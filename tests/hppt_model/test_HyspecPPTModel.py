83fc97dd06b4dd3dc414fa261046b69b38d94655
Kyle Ma <kyleqianlima@gmail.com>
2025-01-09 12:53:54 -0500

    pre-commit fix

--- tests/hppt_model/test_HyspecPPTModel.py
+++ tests/hppt_model/test_HyspecPPTModel.py
@@ -1,7 +1,5 @@
-import numpy as np
-
-import hyspecppt.hppt.hppt_model as hppt_model
-
+import hyspecppt.hppt.hppt_model as hppt_model #F401
+import numpy as np # F401
 
 def test_single_crystal_parameter_set_parameters():
     """Test SingleCrystalParameters set_parameters function"""
