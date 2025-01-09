801cd60d4deca2f88c3c48eb0fc9076a78872ec0
Kyle Ma <kyleqianlima@gmail.com>
2025-01-09 12:55:35 -0500

    precommit fix

--- tests/hppt_model/test_HyspecPPTModel.py
+++ tests/hppt_model/test_HyspecPPTModel.py
@@ -1,5 +1,7 @@
-import hyspecppt.hppt.hppt_model as hppt_model #F401
-import numpy as np # F401
+import numpy as np  # noqa: F401
+
+import hyspecppt.hppt.hppt_model as hppt_model  # noqa: F401
+
 
 def test_single_crystal_parameter_set_parameters():
     """Test SingleCrystalParameters set_parameters function"""
