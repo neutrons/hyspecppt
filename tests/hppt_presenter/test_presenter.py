93bf87a18af19b811fa5d883f4b6d35bc237956a
Patrou, Maria <patroum@ornl.gov>
2025-01-13 13:33:18 -0500

    tests for validators, sc-pd switch, qmod, etc

--- tests/hppt_presenter/test_presenter.py
+++ tests/hppt_presenter/test_presenter.py
@@ -245,3 +245,146 @@ def test_switch_to_powder_qmod_updated_values(hyspec_app, qtbot):
     # Qmod value should be back to its default value
     assert crosshair_widget.modQ_edit.text() == "0.000"
     assert crosshair_widget.modQ_edit.styleSheet() != INVALID_QLINEEDIT
+
+
+def test_handle_field_values_update(hyspec_app, qtbot):
+    """Test switch to Single Crystal check sc parameters"""
+    # show the app
+    hyspec_app.show()
+    qtbot.waitUntil(hyspec_app.show, timeout=5000)
+    assert hyspec_app.isVisible()
+
+    hyspec_view = hyspec_app.main_window.HPPT_view
+    experiment_widget = hyspec_view.SCW
+    crosshair_widget = hyspec_view.CW
+
+    # switch to single crystal
+    hyspec_view.switch_to_SC()
+
+    # set a valid a h value
+    experiment_widget.h_edit.clear()
+    qtbot.keyClicks(experiment_widget.h_edit, "1")
+    assert experiment_widget.h_edit.text() == "1"
+
+    # set a valid a k value
+    experiment_widget.k_edit.clear()
+    qtbot.keyClicks(experiment_widget.k_edit, "2")
+    assert experiment_widget.k_edit.text() == "2"
+
+    # Simulate gaining/losing focus
+    experiment_widget.k_edit.setFocus()
+    qtbot.keyPress(experiment_widget.k_edit, Qt.Key_Return)
+
+    # Qmod value should be updated with the new value
+    assert crosshair_widget.modQ_edit.text() == "14.050"
+    assert crosshair_widget.modQ_edit.styleSheet() != INVALID_QLINEEDIT
+
+
+def test_switch_to_sc_invalid_updated_default(hyspec_app, qtbot):
+    """Test switch to Single Crystal - check sc parameters default values when switching to powder and back"""
+    # show the app
+    hyspec_app.show()
+    qtbot.waitUntil(hyspec_app.show, timeout=5000)
+    assert hyspec_app.isVisible()
+
+    hyspec_view = hyspec_app.main_window.HPPT_view
+    experiment_widget = hyspec_view.SCW
+    crosshair_widget = hyspec_view.CW
+
+    # switch to single crystal
+    hyspec_view.switch_to_SC()
+
+    # set an invalid alpha value
+    qtbot.keyClicks(experiment_widget.alpha_edit, "00")
+    assert experiment_widget.alpha_edit.text() == "900"
+    assert experiment_widget.alpha_edit.styleSheet() == INVALID_QLINEEDIT
+
+    # set an invalid H value
+    experiment_widget.h_edit.clear()
+    assert experiment_widget.h_edit.text() == ""
+    assert experiment_widget.h_edit.styleSheet() == INVALID_QLINEEDIT
+
+    # switch to powder
+    hyspec_view.switch_to_powder()
+
+    # qmod has default value
+    assert crosshair_widget.modQ_edit.text() == "0.000"
+
+    # switch to single crystal
+    hyspec_view.switch_to_SC()
+
+    # single crystals fields back to default values
+    assert experiment_widget.alpha_edit.text() == "90"
+    assert experiment_widget.alpha_edit.styleSheet() != INVALID_QLINEEDIT
+
+    assert experiment_widget.h_edit.text() == "0"
+    assert experiment_widget.h_edit.styleSheet() != INVALID_QLINEEDIT
+
+    # qmod has default value
+    assert crosshair_widget.modQ_edit.text() == "0.000"
+
+
+def test_switch_to_sc_invalid_updated_new(hyspec_app, qtbot):
+    """Test switch to Single Crystal - check sc parameters and qmod new values when switching to powder and back"""
+    # show the app
+    hyspec_app.show()
+    qtbot.waitUntil(hyspec_app.show, timeout=5000)
+    assert hyspec_app.isVisible()
+
+    hyspec_view = hyspec_app.main_window.HPPT_view
+    experiment_widget = hyspec_view.SCW
+    crosshair_widget = hyspec_view.CW
+
+    # switch to single crystal
+    hyspec_view.switch_to_SC()
+
+    # set a valid a k value
+    experiment_widget.h_edit.clear()
+    qtbot.keyClicks(experiment_widget.h_edit, "2.2")
+    assert experiment_widget.h_edit.text() == "2.2"
+
+    # Simulate gaining/losing focus
+    experiment_widget.h_edit.setFocus()
+    qtbot.keyPress(experiment_widget.h_edit, Qt.Key_Return)
+
+    # set an invalid alpha value
+    qtbot.keyClicks(experiment_widget.alpha_edit, "00")
+    assert experiment_widget.alpha_edit.text() == "900"
+    assert experiment_widget.alpha_edit.styleSheet() == INVALID_QLINEEDIT
+
+    # Simulate gaining/losing focus
+    experiment_widget.alpha_edit.setFocus()
+    qtbot.keyPress(experiment_widget.alpha_edit, Qt.Key_Return)
+
+    # set an invalid H value
+    experiment_widget.h_edit.clear()
+    assert experiment_widget.h_edit.text() == ""
+    assert experiment_widget.h_edit.styleSheet() == INVALID_QLINEEDIT
+
+    # Simulate gaining/losing focus
+    experiment_widget.h_edit.setFocus()
+    qtbot.keyPress(experiment_widget.h_edit, Qt.Key_Return)
+
+    # switch to powder
+    hyspec_view.switch_to_powder()
+
+    # qmod has the calculated value
+    assert crosshair_widget.modQ_edit.text() == "13.823"
+    # qmod value updated manually
+    crosshair_widget.modQ_edit.clear()
+    qtbot.keyClicks(crosshair_widget.modQ_edit, "5")
+    assert crosshair_widget.modQ_edit.text() == "5"
+
+    # switch to single crystal
+    hyspec_view.switch_to_SC()
+
+    # single crystals fields back to default values
+    assert experiment_widget.alpha_edit.text() == "90.0"
+    assert experiment_widget.alpha_edit.styleSheet() != INVALID_QLINEEDIT
+
+    # single crystals fields back to the last valid value
+    assert experiment_widget.h_edit.text() == "2.2"
+    assert experiment_widget.h_edit.styleSheet() != INVALID_QLINEEDIT
+
+    # qmod has the calculated value
+    assert crosshair_widget.modQ_edit.text() == "13.823"
